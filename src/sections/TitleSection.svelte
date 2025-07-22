74f3301266930e6026dd023b547f36c2d7c27bd2
Sarah <sarahestrada078@gmail.com>
2025-07-21 22:58:55 -0400

    changing descriptions

--- src/sections/TitleSection.svelte
+++ src/sections/TitleSection.svelte
@@ -1,7 +1,7 @@
 <script>
     import TitleCard from "../lib/TitleCard.svelte";
     
-    const title = "Welcome to the KWK Scrollytelling Template";
+    const title = "Welcome to the *modified* KWK Scrollytelling Template";
     const subtitle = "A journey through data, creativity, and code";
 </script>
 
